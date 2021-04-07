ab7e0ff391a792da990fc72ec5c49bcc61dbb505
sharmaine-c.galola <sharmaine-c.galola@mynt.xyz>
2021-04-07 11:35:50 +0800

    updated day 1 project

--- d1a1-master/d1a1-master/activity/src/main/java/ph/apper/activity/App.java
+++ d1a1-master/d1a1-master/activity/src/main/java/ph/apper/activity/App.java
@@ -3,13 +3,16 @@ package ph.apper.activity;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication application = new SpringApplication(App.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
     @RestController
